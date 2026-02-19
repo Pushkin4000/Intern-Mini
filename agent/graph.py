29fdf2692d7fa910b7860f740143e3940737b944
Pushkin4000 <pushkinranjan@gmail.com>
2026-02-19 16:30:03 +0530

    Created basic planner file

---
+++ agent/graph.py
@@ -0,0 +1,27 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+load_dotenv()
+from pydantic import BaseModel, Field
+
+class File(BaseModel):
+    Path: str = Field(description="The path to the file to be created or modified.")
+    Purpose: str = Field(description="The purpose of the file example:'main application','data module', 'source data', etc.")
+
+class Plan(BaseModel):
+    Name: str = Field(description="The name of the application according to the user request.")
+    description: str = Field(description="The description of the app that wil be build example: A notetaking web application.")
+    techstack:str = Field(description="The requried techstack needed for building that app example:'Python','java','html/css',etc.")
+    features: list[str] = Field(description="A list of features that the application should have example:'user authentication', 'data visualization', etc.")
+    files: list[File] = Field(description="A list of files that are required to build that app, each file should be with a 'path' and 'purpose'.")
+
+
+user_prompt="Create me a simiple dog donation website."
+
+Planner=f"""You are a Planner agent and your task is to convert the user request into a detailed plan. 
+The user Request:{ user_prompt}
+"""
+
+llm=ChatGroq(model="openai/gpt-oss-120b")
+
+resp=llm.with_structured_output(Plan).invoke(Planner)
+print(resp)
